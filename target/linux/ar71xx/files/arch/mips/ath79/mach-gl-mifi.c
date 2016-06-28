c43cf990304636b6af24f5bc7dde319ff7b39e9b
alzhao <alzhao@gmail.com>
2016-06-28 09:53:23 +0800

    update mifi support, add 3G4G modem power control

--- target/linux/ar71xx/files/arch/mips/ath79/mach-gl-mifi.c
+++ target/linux/ar71xx/files/arch/mips/ath79/mach-gl-mifi.c
@@ -27,6 +27,7 @@
 #define GL_MIFI_GPIO_LED_LAN		16
 #define GL_MIFI_GPIO_LED_WLAN		1
 #define GL_MIFI_GPIO_LED_NET		0
+#define GL_MIFI_GPIO_LED_3GCONTROL	7
 
 #define GL_MIFI_GPIO_BTN_RESET    11
 
@@ -59,6 +60,11 @@ static struct gpio_led gl_mifi_leds_gpio[] __initdata = {
 		.gpio = GL_MIFI_GPIO_LED_NET,
 		.active_low = 0,
 	},
+	{
+		.name = "gl_mifi:3gcontrol",
+		.gpio = GL_MIFI_GPIO_LED_3GCONTROL,
+		.active_low = 0,
+	}
 };
 
 static struct gpio_keys_button gl_mifi_gpio_keys[] __initdata = {
